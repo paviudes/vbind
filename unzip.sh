39d7024365cd0a106856f21875be926d2f4f45c0
CharithRaj AdkarPurushothama <charith@ip08.m>
2018-10-17 20:06:35 -0400

    migrate

--- unzip.sh
+++ unzip.sh
@@ -1,6 +1,5 @@
 ## declare an array variable
-# declare -a arr=("IIIweekCtrl_S4_R1_001-TR.fastq.gz" "IIIweekEV_S5_R1_001-TR.fastq.gz" "IIIweekRDR_S6_R1_001-TR.fastq.gz")
-declare -a arr=("IIweekCtrl_S1_R1_001-TR.fastq.gz" "IIweekEV_S2_R1_001-TR.fastq.gz" "IIweekRDR_S3_R1_001-TR.fastq.gz")
+declare -a arr=("IIIweekCtrl_S4_R1_001-TR.fastq.gz" "IIIweekEV_S5_R1_001-TR.fastq.gz" "IIIweekRDR_S6_R1_001-TR.fastq.gz" "IIweekCtrl_S1_R1_001-TR.fastq.gz" "IIweekEV_S2_R1_001-TR.fastq.gz" "IIweekRDR_S3_R1_001-TR.fastq.gz")
 # now loop through the above array
 for file in "${arr[@]}"
 do
