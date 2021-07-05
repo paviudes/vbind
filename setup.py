b458c405b73cdbfc94aeaf3fa09441fad889cd08
Pavi <pavithran.yer@uwaterloo.c>
2021-07-05 09:01:06 -0400

    remove chache files

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name="vbind",
     packages=["vbind"],
-    version="1.0.1",
+    version="1.0.2",
     license="BSD Clause 3",
     description="sRNA profiler",
     long_description="vbind is a userfriedly tool for RNA sequencing. In particular, it can be used to compute and visualize the bindings between a pool of sRNA nuleotides and a genome sequence. Please visit https://paviudes.github.io/vbind/ for more information.",
@@ -15,7 +15,6 @@ setup(
     install_requires=[
         "scipy",
         "numpy",
-        "multiprocessing",
         "matplotlib",
         "tqdm",
         "datetime",
